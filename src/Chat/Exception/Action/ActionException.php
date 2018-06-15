fcb84973054629a3ade4d39d9ea04ad34f0fd59a
Udalych <vovarx@gmail.com>
2018-06-15 16:47:15 +0300

    Fix [Validation]: fix generating validation exception

---
+++ src/Chat/Exception/Action/ActionException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Chat\Exception\Action;
+
+
+class ActionException
+{
+
+}
---
+++ src/Chat/Exception/Action/UserNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Created by PhpStorm.
+ * User: volodymyr
+ * Date: 15.06.18
+ * Time: 16:29
+ */
+
+namespace Chat\Exception\Action;
+
+
+class UserNotFoundException
+{
+
+}
